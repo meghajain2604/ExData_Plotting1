607ff8178b8c327d7c4f08e5630bf6304446eb09
Megha Jain <69403038+meghajain2604@users.noreply.github.com>
2020-08-27 12:32:33 +0530

    Delete plot2.R

--- plot2.R
+++
@@ -1,7 +0,0 @@
-data <- read.csv("household_power_consumption.txt", header = T,sep = ';',na.strings = "?",stringsAsFactors = FALSE , dec = ".")
-
-data1 <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
-date_time <- strptime(paste(data1$Date, data1$Time, sep = " "), "%d/%m/%Y %H:%M:%S
-plot(date_time,data1$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power(kilowatts)")
-dev.copy(png, file = "plot2.png", width = 480, height = 480)
-dev.off()
